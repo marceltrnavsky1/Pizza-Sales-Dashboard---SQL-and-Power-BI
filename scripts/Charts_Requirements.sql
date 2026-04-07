3d8a10cdd3f0ada1bb1df6e9929d3788b2b92fb3
marceltrnavsky1 <marcel.trnavsky1@gmail.com>
2026-04-07 21:08:22 +0200

    Create Charts_Requirements.sql

---
+++ scripts/Charts_Requirements.sql
@@ -0,0 +1,12 @@
+/* We would like to visualize various asptects of our pizza sales data to gain insights and understand key
+trends. We have identified the following requirements for creating charts:
+1.Daily Trend for Total Orders, 2.Hourly Trend for Total Orders, 3.Percentage of Sales by Pizza Category */
+
+/* Daily Trend for Total Orders: Create a bar chart that displays the daily trend of total orders a specific time period.
+This chart will help us identify any patterns or fluctuations in order volumes on a daily basis. */
+
+SELECT
+DATENAME(WEEKDAY, order_date) AS day_of_the_week,
+COUNT(DISTINCT order_id) AS total_orders
+FROM pizza_sales
+GROUP BY DATENAME(WEEKDAY, order_date)
