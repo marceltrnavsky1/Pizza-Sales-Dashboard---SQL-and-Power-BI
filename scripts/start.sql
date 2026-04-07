c29ce185f33efa7cc0b0765a3d11ead0befc49c3
marceltrnavsky1 <marcel.trnavsky1@gmail.com>
2026-04-07 20:12:36 +0200

    Create start.sql

---
+++ scripts/start.sql
@@ -0,0 +1,32 @@
+/* Table overview with time correction */
+SELECT
+pizza_id,
+order_id,
+pizza_name_id,
+quantity,
+order_date,
+CAST (order_time AS TIME(0)) AS order_time,
+unit_price,
+total_price,
+pizza_size,
+pizza_category,
+pizza_ingredients,
+pizza_name
+FROM pizza_sales
+
+/* Handling the NULL before doing any mathematical operations. */
+SELECT *
+FROM pizza_sales
+WHERE 
+    pizza_id IS NULL
+    OR order_id IS NULL
+    OR pizza_name_id IS NULL
+    OR quantity IS NULL
+    OR order_date IS NULL
+    OR order_time IS NULL
+    OR unit_price IS NULL
+    OR total_price IS NULL
+    OR pizza_size IS NULL
+    OR pizza_category IS NULL
+    OR pizza_ingredients IS NULL
+    OR pizza_name IS NULL
